5870d578246513364a2f7dcdf735ad55fab70169
BoostedT <tyler.whitchurch@atlasschool.com>
2024-11-10 19:46:10 -0600

    asdf

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,10 +4,13 @@ TODO list progress"""
 
 import requests
 
+
 def get_todo_list_progress(employee_id):
     # Fetch the employee's information
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    user_url = f"https://jsonplaceholder.typicode.com//
+                users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com//
+                  todos?userId={employee_id}"
 
     #Send GET request to the URLs
     user_response = requests.get(user_url)
@@ -23,21 +26,22 @@ def get_todo_list_progress(employee_id):
     # extract employee's name
     employee_name = user_data.get('name')
 
-    # count the number of completed and pending tasks
+    #task information
     total_tasks = len(todos_data)
-    completed_tasks = [task for task in todos_data if task.get('completed') is True]
-    number_of_completed_tasks = len(completed_tasks)
-
-    print(f"Employee {employee_name} is done with tasks({number_of_completed_tasks}/{total_tasks}):")
-    for task in completed_tasks:
-        print(f"\t " + {task.get('title')})
+    done_tasks = [task for task in todos_data if task.get('completed')]
+    number_of_done_tasks = len(done_tasks)
+    
+    #print the information
+    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+        print("Usage: python3 get_todo_list_progress.py <employee_id>")
     else:
         try:
             employee_id = int(sys.argv[1])
             get_todo_list_progress(employee_id)
         except ValueError:
-            print("Error: Please enter a valid employee ID")
+            print("Error: Please enter a valid employee ID ##")
