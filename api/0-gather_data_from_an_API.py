49c0ba95771245f27d9613494e912cd6fe75c31c
BoostedT <tyler.whitchurch@atlasschool.com>
2024-11-10 20:03:24 -0600

    123

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,46 +2,30 @@
 """Using a REST API, for a given employee ID, returns information about his/her
 TODO list progress"""
 
-import requests
-
-
-def get_todo_list_progress(employee_id):
-    # Fetch the employee's information
-    user_url = f"https://jsonplaceholder.typicode.com//
-                users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com//
-                  todos?userId={employee_id}"
-
-    #Send GET request to the URLs
-    user_response = requests.get(user_url)
-    todos_response = requests.get(todos_url)
-
-    if user_response.status_code == 200 and todos_response.status_code == 200:
-        print("Error: Please enter a valid employee ID")
-        return
-
-    user_data = user_response.json()
-    todos_data = todos_response.json()
-
-    # extract employee's name
-    employee_name = user_data.get('name')
-
-    #task information
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get('completed')]
-    number_of_done_tasks = len(done_tasks)
-    
-    #print the information
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
+from requests import get
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 get_todo_list_progress.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_todo_list_progress(employee_id)
-        except ValueError:
-            print("Error: Please enter a valid employee ID ##")
+   response = get('https://jsonplaceholder.typicode.com/todos/')
+   data = response.json()
+   completed = 0
+   total = 0
+   tasks = []
+   response2 = get('https://jsonplaceholder.typicode.com/users/')
+   data2 = response2.json()
+
+for user in data2:
+    if user.get('id') == int(argv[1]):
+        employee = user.get('name')
+    
+for task in data:
+    if task.get('userId') == int(argv[1]):
+        total += 1
+        if task.get('completed') is True:
+            completed += 1
+            tasks.append(task.get('title'))
+
+print("Employee {} is done with tasks({}/{}):".format(employee, completed, total))
+
+for T in tasks:
+    print("\t {}".format(T))
