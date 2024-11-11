b464f87c933a101cd4b99f738c00828b0c28a017
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-10 21:17:25 -0600

    pep8

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""extended Export data in the JSON format."""
+"""Extended export of data in JSON format for all employees."""
 
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
@@ -11,11 +10,15 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
-            user.get("id"): [{
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": user.get("username")
-            } for task in requests.get(url + "todos",
-                                       params={"userId": user.get("id")}).json()]
+            user.get("id"): [
+                {
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get("username")
+                } for task in requests.get(
+                    url + "todos", params={
+                        "userId": user.get("id")}
+                ).json()
+            ]
             for user in users
-        }, jsonfile)
+        }, jsonfile, indent=4)
