51d5e15ae5313ba7593bdbf75eb1e3201120f835
Tyler <tyler.whitchurch@atlasschool.com>
2024-11-10 21:08:53 -0600

    reverted

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+""" Export data in the JSON format """
 
 import json
 import requests
 import sys
 
-def export_to_JSON():
-    """Export data to JSON format"""
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
             "task": todo.get("title"),
             "completed": todo.get("completed"),
-            "username": username
+            "username": user.get("username")
         } for todo in todos]}, jsonfile)
-
-if __name__ == "__main__":
-    export_to_JSON()
